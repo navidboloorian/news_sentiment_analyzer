827dde6186d21fcf87a308ee61bac0f4bfdd41bf
Navid Boloorian <navidboloorian0@gmail.com>
2024-12-11 17:19:07 -0800

    Added headlines for Wed 12/11/2024

---
+++ site/script.js
@@ -0,0 +1,19 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+const data = await d3.csv("../data.csv");
+
+const createGraph = (source) => {
+  const graphWrapper = document.querySelector("#graph-wrapper");
+  const svg = document.createElement("svg");
+
+  const xAxis = d3.scaleLinear();
+
+  d3.select(svg)
+    .selectAll("circle")
+    .data(data.filter((row) => row.source === source))
+    .join("circle");
+
+  graphWrapper.appendChild(svg);
+};
+
+createGraph("fox");
