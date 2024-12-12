67a1ac7cd9fe0d2bc6034fbf66d890192de39162
Navid Boloorian <navidboloorian0@gmail.com>
2024-12-11 20:57:16 -0800

    Added headlines for Wed 12/11/2024

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -4,14 +4,16 @@ import torch
 def analyze_sentiment(headlines):
   device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
-  analyzer = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest", device=device)
+  analyzer = pipeline("sentiment-analysis", model="siebert/sentiment-roberta-large-english", device=device)
   raw_sentiments = analyzer(headlines)
 
   sentiments = []
 
   for sentiment in raw_sentiments:
+    print(sentiment)
+
     if sentiment["label"] == "negative":
-      sentiments.append(sentiment["score"])
+      sentiments.append(1 - sentiment["score"])
     elif sentiment["label"] == "neutral":
       sentiments.append(1 + sentiment["score"])
     elif sentiment["label"] == "positive":
--- site/script.js
+++ site/script.js
@@ -2,21 +2,101 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const data = await d3.csv("../data.csv");
 
+const sentimentToLabel = (sentiment) => {
+  if (sentiment > 2) {
+    return "Positive";
+  } else if (sentiment > 1) {
+    return "Neutral";
+  } else {
+    return "Negative";
+  }
+};
+
 const createGraph = (source) => {
-  const graphWrapper = document.querySelector("#graph-wrapper");
-  const svg = document.createElement("svg");
+  const width = 750;
+  const height = 750;
+  const margin = { bottom: 50, top: 30, right: 30, left: 30 };
 
-  const width = 500;
-  const height = 500;
+  const paddedWidth = width + margin.left + margin.right;
+  const paddedHeight = height + margin.bottom + margin.top;
 
-  const xAxis = d3.scaleLinear();
+  const svg = d3
+    .select("#graph-wrapper")
+    .append("svg")
+    .attr("width", paddedWidth)
+    .attr("height", paddedHeight)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  d3.select(svg)
+  const tooltip = d3
+    .select("#graph-wrapper")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+  const mouseover = function (event, d) {
+    tooltip.style("opacity", 1);
+  };
+
+  const mousemove = function (event, d) {
+    tooltip
+      .html(
+        `
+        <p class='tooltip-headline'>${d.headline}</p>
+        <p>Sentiment: ${
+          Math.round(d.sentiment * 1000) / 1000
+        } (${sentimentToLabel(d.sentiment)})</p>
+      `
+      )
+      .style("left", event.x + "px")
+      .style("top", event.y + "px");
+  };
+
+  const mouseleave = function (event, d) {
+    tooltip.style("opacity", 0);
+  };
+
+  const xScale = d3
+    .scaleUtc()
+    .domain(
+      d3.extent(
+        data.filter((row) => row.source === source),
+        (d) => new Date(d.date)
+      )
+    )
+    .range([50, width]);
+
+  const yScale = d3.scaleLinear().domain([0, 3]).range([height, 0]);
+
+  const xAxis = d3
+    .axisBottom(xScale)
+    .ticks(d3.utcDay.every(1))
+    .tickFormat((d) => d.toLocaleDateString("en-US"));
+  const yAxis = d3.axisLeft(yScale);
+
+  svg.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+  svg.append("g").call(yAxis);
+
+  svg
+    .append("g")
     .selectAll("circle")
     .data(data.filter((row) => row.source === source))
-    .join("circle");
-
-  graphWrapper.appendChild(svg);
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      return xScale(new Date(d.date));
+    })
+    .attr("cy", function (d) {
+      return yScale(d.sentiment);
+    })
+    .attr("r", 10)
+    .attr("stroke", "black")
+    .style("fill", function (d) {
+      return d.subject === "Republicans" ? "#e81b23" : "#00aef3";
+    })
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave);
 };
 
 createGraph("fox");
